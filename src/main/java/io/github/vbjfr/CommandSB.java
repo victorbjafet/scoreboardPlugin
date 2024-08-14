b5cb0333736a58ade6d4f659f868286df144e73b
Victor Jafet <victorbjafet@gmail.com>
2024-08-14 13:31:35 -0400

    hard coded health2 to not be settable, to allow for health to show in the tab list

--- src/main/java/io/github/vbjfr/CommandSB.java
+++ src/main/java/io/github/vbjfr/CommandSB.java
@@ -1,15 +1,10 @@
 package io.github.vbjfr;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.event.EventHandler;
 import org.bukkit.scoreboard.DisplaySlot;
-import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 
 public class CommandSB implements CommandExecutor {
@@ -23,8 +18,14 @@ public class CommandSB implements CommandExecutor {
         else {
             Scoreboard board = Bukkit.getScoreboardManager().getMainScoreboard();
             if (board.getObjective(args[0]) != null) {
-                board.getObjective(args[0]).setDisplaySlot(DisplaySlot.SIDEBAR);
-                sender.sendMessage("Leaderboard set to " + args[0]);
+                if (!args[0].equals("Health2")) { //hard coded so that the health in the tab menu doesnt go away lol
+                    board.getObjective(args[0]).setDisplaySlot(DisplaySlot.SIDEBAR);
+                    Bukkit.broadcastMessage(sender.getName() + " set the leaderboard to " + args[0]);
+                }
+                else {
+                    sender.sendMessage("Don't change Health2, use Health instead");
+                }
+
             }
             else {
                 sender.sendMessage("Leaderboard " + args[0] + " does not exist");
